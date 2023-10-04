49d251ae8255d0a586217c212c7fa49a10c679da
kritscher33 <110264209+kritscher33@users.noreply.github.com>
2023-10-04 14:55:25 -0700

    Small changes
    Update to get_g_j for scenario 6, stoped having x be 0 for scenario 6

--- qatf.R
+++ qatf.R
@@ -9,15 +9,27 @@ library(detrendr)
 # trace(get_model, edit=TRUE)
 library(glmgen)
 
+<<<<<<< Updated upstream
 rm(list = ls())
+=======
+# rm(list = ls())
+# cum_data <- dat.frame()
+>>>>>>> Stashed changes
 
 MSE <- function(a, b){
   len = length(a)
   return(norm(a - b, type="2")**2/len) 
 }
 
+<<<<<<< Updated upstream
 tau <- c(0.5)
 n <- 1000
+=======
+
+
+tau <- c(0.9)
+n <-500
+>>>>>>> Stashed changes
 d <- 10
 
 
@@ -38,21 +50,19 @@ x <- x/n
 # get scenerio 5
 # x<- cos(6*pi*x/n)
 
-# get scenario 6
-# x<- x*0
 
 # get the simulated heterogeneously-smooth data
 x_j <- x # we could add permutation for different j.
 
 
 # scenerio 6
-# get_g_j <- function(x_j, j){
-#   g_0 <- (x_j + 0.1)*(j/10)
-#   g_0_n <- norm(g_0, type="2")/sqrt(n)
-#   a_j <- 1/g_0_n
-#   g_0 <- a_j**(x_j + 0.1)*(j/10)
-#   return(g_0)
-# }
+get_g_j <- function(x_j, j){
+  g_0 <- (x_j*0 + 0.1)*(j/10)
+  g_0_n <- norm(g_0, type="2")/sqrt(n)
+  a_j <- 1/g_0_n
+  g_0 <- a_j**(x_j*0 + 0.1)*(j/10)
+  return(g_0)
+}
 
 # scenerio 5
 # get_g_j <- function(x_j, j){
@@ -75,13 +85,13 @@ x_j <- x # we could add permutation for different j.
 
 
 # scenerio 1, 2, 3, Doppler-like
-get_g_j <- function(x_j, j){
-  g_0 <- sin(2*pi/(x_j + 0.1)**(j/10))
-  g_0_n <- norm(g_0, type="2")/sqrt(n)
-  a_j <- 1/g_0_n
-  g_0 <- a_j*sin(2*pi/(x_j + 0.1)**(j/10))
-  return(g_0)
-}
+# get_g_j <- function(x_j, j){
+#   g_0 <- sin(2*pi/(x_j + 0.1)**(j/10))
+#   g_0_n <- norm(g_0, type="2")/sqrt(n)
+#   a_j <- 1/g_0_n
+#   g_0 <- a_j*sin(2*pi/(x_j + 0.1)**(j/10))
+#   return(g_0)
+# }
 
 
 
@@ -100,27 +110,75 @@ y_list <- unname(y_list)
 y_star <- colSums(y_list)
 #get the y_i
 # get other scenerios error
+<<<<<<< Updated upstream
 sce1 <- rnorm(n, 0, 1)
 sce2 <- rcauchy(n, 0, 1)
 # e <- x**0.5/n**0.5
+=======
+# sce1 <- rnorm(n, 0, 1)
+# sce2 <- rcauchy(n, 0, 1)
+# e <- (x**.5)  # i**.5 / n**.5
+>>>>>>> Stashed changes
 # te <- rt(n, 2)
 # sce3 <- e*te
 # sce4 <- rt(n,3)
 # sce5 <- rcauchy(n, 0, 1)
-y <- y_star + sce1
+# y <- y_star + sce5
 
 # get scenerio 6 error
-# half <- n/2
-# x[1:half] <- (0.25*(x[1:half]/n)**0.5 + 1.375)/3
-# x <- replace(x, seq(half+1, n, 1), (7*(tail(x,half)/n)**0.5-2)/3)
-# sce6 <-rt(n,2)
-# sce6_9q <- qt(0.9, 2)
-# y<-y_star+x*sce6
-# y_9q <- y_star+x*sce6_9q
-# plot(y_9q, type="l", col="black", ylab='0.9 quantile')
+half <- n/2
+x[1:half] <- (0.25*(x[1:half]/n)**0.5 + 1.375)/3
+x <- replace(x, seq(half+1, n, 1), (7*(tail(x,half)/n)**0.5-2)/3)
+sce6 <-rt(n,2)
+sce6_9q <- qt(0.9, 2)
+y<-y_star+x*sce6
+y_star <- y_star+x*sce6_9q
+plot(y_star, type="l", col="black", ylab='0.9 quantile')
 
 
 
+<<<<<<< Updated upstream
+=======
+
+
+
+ATF2 <- get_mse(2)
+ATF3 <- get_mse(3)
+QATF2 <- get_mse_q(2)
+QATF3 <- get_mse_q(3)
+
+cat(ATF2$MSE, "at lambda : ", ATF2$LAMBDA, "\n")
+cat(ATF3$MSE, "at lambda : ", ATF3$LAMBDA, "\n")
+cat(QATF2$MSE, "at lambda : ", QATF2$LAMBDA, "\n")
+cat(QATF3$MSE, "at lambda : ", QATF3$LAMBDA, "\n")
+
+
+cum_data <- rbind(cum_data, data.frame(n = n, Scenario = 1,
+                                       tau=tau, QATF2 = QATF2$MSE, QATF3 = QATF3$MSE,
+                                       ATF2 =ATF2$MSE, ATF3 =ATF3$MSE))
+
+# write data to a sample.csv file
+write.csv(cum_data, file = "sce1-5.csv")
+
+
+
+
+#> Using same lambda for all quantiles
+plot(y, type="l", col="black") # this is the y with errors
+plot(y_star, type="l", col="black") # this is the y_star, without errors
+
+
+# lines(trend_hat~x, col="red")
+# lines(q_trend_hat~x, col="blue")
+# lines(trend[,1]~x, col="red")
+# lines(trend[,2]~x, col="blue")
+
+
+
+
+
+
+>>>>>>> Stashed changes
 # Worth passing in x, y, and y_star? 
 get_mse <- function(k, alpha = 10**-6, max_t = 50){
   lambda_list <- 10**seq(5, -10, length.out=100)
