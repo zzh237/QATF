d1034530937faad56b626b9ffc44d53a85b9c2e5
kritscher33 <110264209+kritscher33@users.noreply.github.com>
2024-09-20 09:08:31 -0700

    Happiness Data Work
    All the cleaning and statistical inference code for happiness data.
    Also, organization.

--- real_data.R
+++ real_data.R
@@ -1,8 +1,8 @@
 # QATF Real data
 # real_data.R
 
-source(algorithms.R)
-
+source("algorithms.R")
+library(tidyverse)
 
 # TODO:
 # Set up conformal inference and prediction interval functions
@@ -10,14 +10,162 @@ source(algorithms.R)
 # Construct plots using this data 
 
 # https://arxiv.org/abs/2012.01758
-housing <- read_csv("housing.csv")
-
 # Bike data, and other datasets, from https://arxiv.org/abs/1909.05433
-bikeday <- read_csv("bike_data/day.csv")
-bikehour <- read_csv("bike_data/hour.csv")
-
-# Happiness data: https://arxiv.org/pdf/1409.5391
-
 # Probablistic Load Forecasting https://arxiv.org/abs/1707.03307
 
-# 
+# Happiness data inspired by: https://arxiv.org/pdf/1409.5391
+
+Combined_dat <- read.csv("happiness/Combined_dat.csv")
+par(mfrow = c(3, 3), mar = c(2, 2, 2, 2), oma = c(0, 0, 0, 0))
+for (i in 1:9) {
+  # Generate the plot with the title set to the column name
+  plot(sort(Combined_dat[, 1 + i]), 
+       main = colnames(Combined_dat)[1 + i], 
+       xlab = "", ylab = "", lwd = 1)
+}
+
+min_vals <- Combined_dat %>%
+  select(2:10) %>%
+  summarise(across(everything(), min, na.rm = TRUE)) %>%
+  unlist()
+
+max_vals <- Combined_dat %>%
+  select(2:10) %>%
+  summarise(across(everything(), max, na.rm = TRUE)) %>%
+  unlist()
+
+Scaled_Centered <- Combined_dat %>% 
+  mutate(across(2:10, ~ (. - min_vals[cur_column()]) / (max_vals[cur_column()] - min_vals[cur_column()]))) %>%
+  mutate(Ladder.score = Ladder.score - mean(Ladder.score, na.rm = TRUE))
+
+# For avg test set 0.5 MSE and 0.1, 0.9 Coverage
+splits <- sample(rep(1:10, length.out = nrow(Scaled_Centered)))
+test_set_means <- numeric(10)
+test_set_coverages9 <- numeric(10)
+test_set_coverages8 <- numeric(10)
+test_set_coverages6 <- numeric(10)
+
+for (i in 1:10) {
+  
+  test_indices <- which(splits == i)
+  test_data <- Scaled_Centered[test_indices, ]
+  train_data <- Scaled_Centered[-test_indices, ]
+  
+  n = nrow(train_data)
+  
+  res5 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.5, 0, prints = FALSE)
+  lambda5 <- res5$LAMBDAS[which.min(res5$MEANS)]
+  fit5 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.5, 0, lambda5)
+  test_set_means[i] <- MSE(predict_fit(train_data[, 2:10], fit5$fit, test_data[, 2:10]), test_data[, 11])
+  
+  res95 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.95, 0, prints = FALSE)
+  lambda95 <- res95$LAMBDAS[which.min(res95$MEANS)]
+  fit95 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.95, 0, lambda95)
+  pred95 <- predict_fit(train_data[, 2:10], fit95$fit, test_data[, 2:10])
+  
+  res9 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.9, 0, prints = FALSE)
+  lambda9 <- res9$LAMBDAS[which.min(res9$MEANS)]
+  fit9 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.9, 0, lambda9)
+  pred9 <- predict_fit(train_data[, 2:10], fit9$fit, test_data[, 2:10])
+  
+  res8 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.8, 0, prints = FALSE)
+  lambda8 <- res8$LAMBDAS[which.min(res8$MEANS)]
+  fit8 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.8, 0, lambda8)
+  pred8 <- predict_fit(train_data[, 2:10], fit8$fit, test_data[, 2:10])
+  
+  res2 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.2, 0, prints = FALSE)
+  lambda2 <- res2$LAMBDAS[which.min(res2$MEANS)]
+  fit2 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.2, 0, lambda2)
+  pred2 <- predict_fit(train_data[, 2:10], fit2$fit, test_data[, 2:10])
+  
+  res1 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.1, 0, prints = FALSE)
+  lambda1 <- res1$LAMBDAS[which.min(res1$MEANS)]
+  fit1 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.1, 0, lambda1)
+  pred1 <- predict_fit(train_data[, 2:10], fit1$fit, test_data[, 2:10])
+  
+  res05 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.05, 0, prints = FALSE)
+  lambda05 <- res05$LAMBDAS[which.min(res05$MEANS)]
+  fit05 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.05, 0, lambda05)
+  pred05 <- predict_fit(train_data[, 2:10], fit05$fit, test_data[, 2:10])
+  
+  test_set_coverages9[i] <- mean(test_data[, 11] <= pred95 & test_data[, 11] >= pred05)
+  test_set_coverages8[i] <- mean(test_data[, 11] <= pred9 & test_data[, 11] >= pred1)
+  test_set_coverages6[i] <- mean(test_data[, 11] <= pred8 & test_data[, 11] >= pred2)
+  
+  cat("\n\nIteration ", i, " finished\n\n",
+      "lambda_med = ", lambda_med, "\n",
+      "lambda_low = ", lambda_low, "\n",
+      "lambda_up = ", lambda_up, "\n", 
+      "test_set_means[", i, "] = ", test_set_means[i], "\n",
+      "test_set_coverages[", i, "] = ", test_set_coverages[i], "\n\n\n", sep = "")
+}
+
+
+mean(test_set_means)
+mean(test_set_coverages9)
+mean(test_set_coverages8)
+mean(test_set_coverages6)
+
+# qatf_cv <- (x, y, n, d, tau, k, alpha = 10**-4, nfolds = 5, max_t = 50, prints = FALSE)
+# predict_fit <- function(data, fit, loc, method = "1-NN")
+# fit_qatf <- (x, y, n, d, tau, k, lambda, alpha = 10**-4, max_t = 50)
+
+
+# Plots
+n = nrow(Scaled_Centered)
+lambda_qres <- qatf_cv(t(Scaled_Centered[, 2:10]), Scaled_Centered[, 11], n, 9, 0.5, 0)
+lambda <- lambda_qres$LAMBDAS[which.min(lambda_qres$MEANS)]
+outq <- fit_qatf(t(Scaled_Centered[, 2:10]), Scaled_Centered[, 11], n, 9, 0.5, 0, lambda)
+
+for (i in 1:9) {
+  # Generate the plot
+  plot(outq[[2]][, i][outq[[3]][i, ]] + mean(Combined_dat[, 11]), 
+       xaxt = 'n', yaxt = 'n', main = paste("Plot", i), xlab = "", ylab = "", lwd = 1,
+       ylim = c(0, 10))
+  
+  # # If I need labels
+  # # Calculate the original x-axis labels for this plot
+  # x_original <- seq(0, 1, length.out = 5) * (max_vals[i - 1] - min_vals[i - 1]) + min_vals[i - 1]
+  # # Add custom x-axis labels with the original values
+  # axis(1, at = seq(0, 1, length.out = 5), labels = round(x_original, 2))
+}
+
+
+
+
+
+# Their predictors were: 
+# Satisfaction with freedom of choice
+# Satisfaction with job
+# Statisfaction with community
+# Trust in national government
+# % Rural population
+# % Females with secondary education
+# Mortality rate, under 5
+# Log gross national income
+# % Labor force unemployed
+# Life expectancy at birth (years) 
+# % internet users
+# Lof sceintific journal articles published
+
+# Our predictors
+# Log GDP per-capita (WH)
+# Social Support (WH)
+# Healthy life expectancy at birth (WH)
+# Freedom to make life choices (WH)
+# Perceptions of corruption (WH)
+# Years of Compulsory Education (WDI)
+# % Rural population (WDI)
+# Log Scientific journal articles published (WDI)
+# % internet users (WDI)
+
+
+
+
+
+
+
+
+# BIC to find lambda and then fit median to verify it is close to the mean
+# Prediction interval with train test (.1 and .9 tau, coverage should be 80%)
+# To evaluate coverage, evaluate closest x in training and see if it has the y 
