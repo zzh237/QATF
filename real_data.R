83c6032200ff603462c21401846a490715f03255
kritscher33 <110264209+kritscher33@users.noreply.github.com>
2024-10-06 14:46:57 -0700

    add sorting options

--- real_data.R
+++ real_data.R
@@ -50,58 +50,79 @@ results <- foreach(i = 1:10) %dopar% {
   res5 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.5, 0, prints = FALSE)
   lambda5 <- res5$LAMBDAS[which.min(rev(res5$MEANS))]
   fit5 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.5, 0, lambda5)
-  test_set_mean <- MSE(predict_fit(train_data[, 2:10], fit5$fit, test_data[, 2:10]), test_data[, 11])
+  pred5 <- predict_fit(train_data[, 2:10], fit5$fit, test_data[, 2:10])
   
-  # # Coverage calculations
-  # res95 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.95, 0, prints = FALSE)
-  # lambda95 <- res95$LAMBDAS[which.min(res95$MEANS)]
-  # fit95 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.95, 0, lambda95)
-  # pred95 <- predict_fit(train_data[, 2:10], fit95$fit, test_data[, 2:10])
-  # 
-  # res9 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.9, 0, prints = FALSE)
-  # lambda9 <- res9$LAMBDAS[which.min(res9$MEANS)]
-  # fit9 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.9, 0, lambda9)
-  # pred9 <- predict_fit(train_data[, 2:10], fit9$fit, test_data[, 2:10])
-  # 
-  # res8 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.8, 0, prints = FALSE)
-  # lambda8 <- res8$LAMBDAS[which.min(res8$MEANS)]
-  # fit8 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.8, 0, lambda8)
-  # pred8 <- predict_fit(train_data[, 2:10], fit8$fit, test_data[, 2:10])
-  # 
-  # res2 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.2, 0, prints = FALSE)
-  # lambda2 <- res2$LAMBDAS[which.min(res2$MEANS)]
-  # fit2 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.2, 0, lambda2)
-  # pred2 <- predict_fit(train_data[, 2:10], fit2$fit, test_data[, 2:10])
-  # 
-  # res1 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.1, 0, prints = FALSE)
-  # lambda1 <- res1$LAMBDAS[which.min(res1$MEANS)]
-  # fit1 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.1, 0, lambda1)
-  # pred1 <- predict_fit(train_data[, 2:10], fit1$fit, test_data[, 2:10])
-  # 
-  # res05 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.05, 0, prints = FALSE)
-  # lambda05 <- res05$LAMBDAS[which.min(res05$MEANS)]
-  # fit05 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.05, 0, lambda05)
-  # pred05 <- predict_fit(train_data[, 2:10], fit05$fit, test_data[, 2:10])
-  # 
-  # test_set_coverage9 <- mean(test_data[, 11] <= pred95 & test_data[, 11] >= pred05)
-  # test_set_coverage8 <- mean(test_data[, 11] <= pred9 & test_data[, 11] >= pred1)
-  # test_set_coverage6 <- mean(test_data[, 11] <= pred8 & test_data[, 11] >= pred2)
+  # Coverage calculations
+  res95 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.95, 0, prints = FALSE)
+  lambda95 <- res95$LAMBDAS[which.min(res95$MEANS)]
+  fit95 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.95, 0, lambda95)
+  pred95 <- predict_fit(train_data[, 2:10], fit95$fit, test_data[, 2:10])
+
+  res9 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.9, 0, prints = FALSE)
+  lambda9 <- res9$LAMBDAS[which.min(res9$MEANS)]
+  fit9 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.9, 0, lambda9)
+  pred9 <- predict_fit(train_data[, 2:10], fit9$fit, test_data[, 2:10])
+
+  res8 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.8, 0, prints = FALSE)
+  lambda8 <- res8$LAMBDAS[which.min(res8$MEANS)]
+  fit8 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.8, 0, lambda8)
+  pred8 <- predict_fit(train_data[, 2:10], fit8$fit, test_data[, 2:10])
+
+  res2 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.2, 0, prints = FALSE)
+  lambda2 <- res2$LAMBDAS[which.min(res2$MEANS)]
+  fit2 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.2, 0, lambda2)
+  pred2 <- predict_fit(train_data[, 2:10], fit2$fit, test_data[, 2:10])
+
+  res1 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.1, 0, prints = FALSE)
+  lambda1 <- res1$LAMBDAS[which.min(res1$MEANS)]
+  fit1 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.1, 0, lambda1)
+  pred1 <- predict_fit(train_data[, 2:10], fit1$fit, test_data[, 2:10])
+
+  res05 <- qatf_cv(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.05, 0, prints = FALSE)
+  lambda05 <- res05$LAMBDAS[which.min(res05$MEANS)]
+  fit05 <- fit_qatf(t(train_data[, 2:10]), train_data[, 11], n, 9, 0.05, 0, lambda05)
+  pred05 <- predict_fit(train_data[, 2:10], fit05$fit, test_data[, 2:10])
   
-  # list(
-  #   test_set_mean = test_set_mean,
-  #   lambda5 = lambda5,
-  #   test_set_coverage9 = test_set_coverage9,
-  #   test_set_coverage8 = test_set_coverage8,
-  #   test_set_coverage6 = test_set_coverage6
-  # )
+  test_set_mean <- MSE(pred5, test_data[, 11])
+  test_set_coverage9 <- mean(test_data[, 11] <= pred95 & test_data[, 11] >= pred05)
+  test_set_coverage8 <- mean(test_data[, 11] <= pred9 & test_data[, 11] >= pred1)
+  test_set_coverage6 <- mean(test_data[, 11] <= pred8 & test_data[, 11] >= pred2)
   
+  
+  pred_matrix <- cbind(pred95, pred9, pred8, pred5, pred2, pred1, pred05)
+  pred_sorted <- t(apply(pred_matrix, 1, sort, decreasing = TRUE))
+  
+  pred95_sorted <- pred_sorted[, 1]
+  pred9_sorted  <- pred_sorted[, 2]
+  pred8_sorted  <- pred_sorted[, 3]
+  pred5_sorted  <- pred_sorted[, 4]
+  pred2_sorted  <- pred_sorted[, 5]
+  pred1_sorted  <- pred_sorted[, 6]
+  pred05_sorted <- pred_sorted[, 7]
+  
+  test_set_mean_sorted <- MSE(pred5, test_data[, 11])
+  test_set_coverage9_sorted <- mean(test_data[, 11] <= pred95_sorted & test_data[, 11] >= pred05_sorted)
+  test_set_coverage8_sorted <- mean(test_data[, 11] <= pred9_sorted & test_data[, 11] >= pred1_sorted)
+  test_set_coverage6_sorted <- mean(test_data[, 11] <= pred8_sorted & test_data[, 11] >= pred2_sorted)
+  
+  
+
   list(
+    lambda5 = lambda5,
+    lambda5 = lambda1,
+    lambda5 = lambda9,
     test_set_mean = test_set_mean,
-    lambda5 = lambda5
+    test_set_coverage9 = test_set_coverage9,
+    test_set_coverage8 = test_set_coverage8,
+    test_set_coverage6 = test_set_coverage6, 
+    test_set_mean_sorted = test_set_mean_sorted,
+    test_set_coverage9_sorted = test_set_coverage9_sorted,
+    test_set_coverage8_sorted = test_set_coverage8_sorted,
+    test_set_coverage6_sorted = test_set_coverage6_sorted
   )
 
-  # cat("Average MSE: ", mean_mse, "\n", 
-  #     "Lambda: ", lambda5, "\n")
+  cat("Average MSE: ", mean_mse, "\n",
+      "Lambda: ", lambda5, "\n")
   
 }
 
@@ -110,27 +131,34 @@ stopCluster(cl)
 
 # Collect and calculate the average MSE and coverage metrics
 mean_mse <- mean(sapply(results, function(x) x$test_set_mean))
-print(mean_mse)
-sapply(results, function(x) x$lambda5)
-# mean_coverage9 <- mean(sapply(results, function(x) x$test_set_coverage9))
-# mean_coverage8 <- mean(sapply(results, function(x) x$test_set_coverage8))
-# mean_coverage6 <- mean(sapply(results, function(x) x$test_set_coverage6))
+mean_coverage9 <- mean(sapply(results, function(x) x$test_set_coverage9))
+mean_coverage8 <- mean(sapply(results, function(x) x$test_set_coverage8))
+mean_coverage6 <- mean(sapply(results, function(x) x$test_set_coverage6))
+mean_mse_sorted <- mean(sapply(results, function(x) x$test_set_mean_sorted))
+mean_coverage9_sorted <- mean(sapply(results, function(x) x$test_set_coverage9_sorted))
+mean_coverage8_sorted <- mean(sapply(results, function(x) x$test_set_coverage8_sorted))
+mean_coverage6_sorted <- mean(sapply(results, function(x) x$test_set_coverage6_sorted))
 
-# # Convert the results into a data frame
-# average_metrics <- data.frame(
-#   Metric = c("Mean MSE", "Mean Coverage 0.9", "Mean Coverage 0.8", "Mean Coverage 0.6"),
-#   Value = c(mean_mse, mean_coverage9, mean_coverage8, mean_coverage6)
-# )
-# write.csv(average_metrics, "average_metrics.csv", row.names = FALSE)
+# Convert the results into a data frame
+average_metrics <- data.frame(
+  Metric = c("Mean MSE", "Mean Coverage 0.9", "Mean Coverage 0.8", "Mean Coverage 0.6", 
+             "Mean MSE (sorted)", "Mean Coverage 0.9 (sorted)", 
+             "Mean Coverage 0.8 (sorted)", "Mean Coverage 0.6 (sorted)"),
+  Value = c(mean_mse, mean_coverage9, mean_coverage8, mean_coverage6, 
+            mean_mse_sorted, mean_coverage9_sorted, mean_coverage8_sorted, mean_coverage6_sorted)
+)
+write.csv(average_metrics, "average_metrics.csv", row.names = FALSE)
 
-# write.csv(data.frame(Split = splits), file = "splits.csv", row.names = FALSE)
-# lambda5_df <- data.frame(Lambda5 = sapply(results, function(x) x$lambda5))
-# write.csv(lambda5_df, file = "lambda5.csv", row.names = FALSE)
+write.csv(data.frame(Split = splits), file = "splits.csv", row.names = FALSE)
+lambda_df <- data.frame(Lambda5 = sapply(results, function(x) x$lambda5), 
+                        Lambda1 = sapply(results, function(x) x$lambda1),
+                        Lambda9 = sapply(results, function(x) x$lambda9))
+write.csv(lambda_df, file = "lambda.csv", row.names = FALSE)
 
-# cat("Average MSE: ", mean_mse, "\n",
-#     "Average Coverage (95% CI): ", mean_coverage9, "\n",
-#     "Average Coverage (90% CI): ", mean_coverage8, "\n",
-#     "Average Coverage (80% CI): ", mean_coverage6, "\n")
+cat("Average MSE: ", mean_mse, "\n",
+    "Average Coverage (95% CI): ", mean_coverage9, "\n",
+    "Average Coverage (90% CI): ", mean_coverage8, "\n",
+    "Average Coverage (80% CI): ", mean_coverage6, "\n")
 
 
 
@@ -159,4 +187,6 @@ sapply(results, function(x) x$lambda5)
 # % Rural population (WDI)
 # Log Scientific journal articles published (WDI)
 # % internet users (WDI)
+
+
 
